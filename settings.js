b42a7712a4f9ba17c26f9baaa4a3ef82dfdca4ac
Lelouch7k <112183817+Lelouch7k@users.noreply.github.com>
2022-09-22 14:06:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['905347856051'] //ur owner number
+global.ownername = "Lelouch" //ur owner name
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['905347856051']
+global.ownertag = '905347856051' //ur tag number
+global.botname = 'Lelouch Lamperouge' //ur bot name
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "🖤" //ur menu react emoji
+global.themeemoji = "🖤" //ur theme emoji
+global.packname = "🖤..Lelouch🃏" //ur sticker watermark packname
+global.author = "ᴸᵉˡᵒᵘᶜʰ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
